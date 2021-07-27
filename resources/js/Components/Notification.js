a588053d6975a9cea581c49813275065eb08f226
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-07-28 01:47:32 +0530

    Add notification component

---
+++ resources/js/Components/Notification.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(2),
+        },
+    },
+}));
+
+/**
+ * @param message Message that is to be displayed
+ * @param severity Possible values - `success | error | warning | info`
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Notification({message, severity}) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity={severity}>
+                    {message}
+                </Alert>
+            </Snackbar>
+        </div>
+    );
+}
