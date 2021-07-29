ca8d85bf5b234b85b9b4134c9eb55b56ec7b2fce
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-07-29 21:26:40 +0530

    Add dummy plan activity and users component

---
+++ resources/js/Pages/Plans/PlanActivity.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PlanActivity() {
+    return (
+        <div>Plan Activity will be listed here.</div>
+    )
+}
---
+++ resources/js/Pages/Plans/PlanUsers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PlanUsers() {
+    return (
+        <div>Plan Users will be listed here.</div>
+    )
+}
