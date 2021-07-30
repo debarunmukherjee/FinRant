8731711caf6abf765c3c363ac1668530efa864de
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-07-31 03:19:42 +0530

    Change request param key for category name
    - This was required to show field specific error messages for different input fields - inertia creates the error object with these request param names as the keys.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,33 +6,30 @@ use App\Models\ExpendCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Validation\Rule;
 
 class CategoryController extends Controller
 {
     public function createCategory(Request $request)
     {
         $userId = Auth::id();
-        $categoryName = $request->post('name');
+        $categoryName = $request->post('categoryName');
 
-        $request->validate(
-            [
-                'name' => [
-                    'required',
-                    'max:50',
-                    'string',
-                    Rule::unique('expend_categories')->where(function ($query) use($userId, $categoryName){
-                        return $query->where([
-                            ['name', $categoryName],
-                            ['created_by', $userId]
-                        ]);
-                    })
-                ]
-            ],
-            [
-                'name.unique' => 'Another category with the same name has been created.'
+        $request->validate([
+            'categoryName' => [
+                'required',
+                'max:50',
+                'string',
+                function ($attribute, $value, $fail) use($userId) {
+                    $categoryExists = ExpendCategory::where([
+                                            ['name', $value],
+                                            ['created_by', $userId]
+                                        ])->exists();
+                    if ($categoryExists) {
+                        $fail('Another category with the same name has been created.');
+                    }
+                },
             ]
-        );
+        ]);
 
         $category = ExpendCategory::create([
             'name' => $categoryName,
