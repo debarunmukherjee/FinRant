bd23cc4c21cb02a2420bb172a73c80454d8d9da5
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-07-31 03:34:33 +0530

    Add danger alert support for modal

--- resources/js/Components/Modal.js
+++ resources/js/Components/Modal.js
@@ -2,7 +2,7 @@ import { Fragment, useRef } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { ExclamationIcon } from '@heroicons/react/outline'
 
-export default function Modal({ open, setOpen, title, children, actionText, onClickAction }) {
+export default function Modal({ open, setOpen, title, children, actionText, onClickAction, isDangerAction = false }) {
     const cancelButtonRef = useRef(null)
 
     return (
@@ -43,9 +43,14 @@ export default function Modal({ open, setOpen, title, children, actionText, onCl
                     >
                         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                <div className="sm:items-start">
+                                <div className="sm:flex sm:items-start">
+                                    {isDangerAction ? (
+                                        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                            <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                        </div>
+                                    ) : ''}
                                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-semibold text-gray-900">
                                             {title}
                                         </Dialog.Title>
                                         <div className="mt-2">
@@ -57,7 +62,7 @@ export default function Modal({ open, setOpen, title, children, actionText, onCl
                             <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                                 <button
                                     type="button"
-                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                    className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-${isDangerAction ? 'red' : 'blue'}-600 text-base font-medium text-white hover:bg-${isDangerAction ? 'red' : 'blue'}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm`}
                                     onClick={onClickAction}
                                 >
                                     {actionText}
