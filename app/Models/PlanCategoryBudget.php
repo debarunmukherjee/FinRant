e46c2f6a100ddf5a0519222b7947bf24ea6e7fd4
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-07-31 03:21:13 +0530

    Add function to get budget list of a user for a plan

--- app/Models/PlanCategoryBudget.php
+++ app/Models/PlanCategoryBudget.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class PlanCategoryBudget extends Model
 {
@@ -15,4 +16,15 @@ class PlanCategoryBudget extends Model
         'user_id',
         'plan_id',
     ];
+
+    public static function getBudgetListForCurrentPlan($planId)
+    {
+        return self::select('name', 'amount')
+            ->join('expend_categories', 'expend_categories.id', '=', 'plan_category_budgets.category_id')
+            ->where([
+                ['plan_category_budgets.plan_id', $planId],
+                ['plan_category_budgets.user_id', Auth::id()]
+            ])
+            ->get();
+    }
 }
