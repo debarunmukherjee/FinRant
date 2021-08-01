44c241a67a9ee7502b69eee6b0e63c4e8539976b
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-01 19:58:13 +0530

    Add util function to get user id from email

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function getUserIdFromEmail($email)
+    {
+        return self::select('id')->where('email', $email)->first()->id;
+    }
 }
