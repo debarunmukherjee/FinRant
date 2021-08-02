bec903c0d1b616f44ccca62219c70224156592e1
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-02 22:29:04 +0530

    Pass members data for a plan

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -6,6 +6,7 @@ use App\Models\ExpendCategory;
 use App\Models\Plan;
 use App\Models\PlanCategoryBudget;
 use App\Models\PlanMember;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
@@ -30,7 +31,32 @@ class PlanController extends Controller
         $plan = Plan::getPlanDetails($planId);
         $budgetList = PlanCategoryBudget::getBudgetListForCurrentPlan($planId);
         $categoryList = ExpendCategory::getAllCategoriesForCurrentUser();
-        return Inertia::render('Plans/ViewPlan', ['planDetails' => $plan, 'budgetList' => $budgetList, 'categoryList' => $categoryList]);
+        $planRole = $isUserPlanMember ? 'member' : 'creator';
+        $planMemberList = PlanMember::getPlanMembersDetailsList($planId);
+        $creatorUserDetails = User::getUserDetails($planCreatorId);
+        $planMemberList[] = [
+            'full_name' => $creatorUserDetails['full_name'],
+            'email' => $creatorUserDetails['email'],
+            'avatar' => $creatorUserDetails['avatar'],
+            'role' => 'creator'
+        ];
+        $formattedPlanMemberList = [];
+        foreach ($planMemberList as $member) {
+            $res['fullName'] = $member['full_name'];
+            $res['email'] = $member['email'];
+            $res['avatar'] = asset('storage/images/' . $member['avatar']);
+            $res['role'] = $member['role'];
+            $formattedPlanMemberList[] = $res;
+        }
+        return Inertia::render('Plans/ViewPlan',
+            [
+                'planDetails' => $plan,
+                'budgetList' => $budgetList,
+                'categoryList' => $categoryList,
+                'planRole' => $planRole,
+                'planMemberList' => $formattedPlanMemberList
+            ]
+        );
     }
 
     public function createPlan(Request $request)
