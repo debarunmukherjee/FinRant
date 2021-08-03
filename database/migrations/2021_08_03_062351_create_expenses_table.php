3db129e17263043ab37d2d84359add67ec038449
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-04 04:17:22 +0530

    Add expense model and migration

---
+++ database/migrations/2021_08_03_062351_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plan_id')->references('id')->on('plans');
+            $table->foreignId('category_id')->references('id')->on('expend_categories');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->boolean('is_shared')->default(0);
+            $table->unsignedFloat('amount');
+            $table->unique(['plan_id', 'category_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
