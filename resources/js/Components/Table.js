37a091426c1ba3f98109da85b9ecf1cb194e2b96
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-06 03:27:47 +0530

    Add table component

---
+++ resources/js/Components/Table.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Table({ headers, children }) {
+    return (
+        <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                            <tr>
+                                {headers.map((header, index) => (
+                                    <th
+                                        key={`table-head-${index+1}`}
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        {header}
+                                    </th>
+                                ))}
+                            </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                            {children}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
