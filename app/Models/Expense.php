394e1905924b49d77533a44184dcb2bb6593b690
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-05 15:52:13 +0530

    Add methods to create shared and unshared expenses

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -13,5 +13,56 @@ class Expense extends Model
         'plan_id',
         'category_id',
         'user_id',
+        'is_shared',
+        'amount',
+        'transaction_id',
+        'shared_expense_batch_id'
     ];
+
+    /**
+     * Create a new expense item for a user, which is unshared by other members.
+     * @param $userId
+     * @param $planId
+     * @param $categoryId
+     * @param $amount
+     * @return bool
+     */
+    public static function createUnsharedExpenseForUser($userId, $planId, $categoryId, $amount): bool
+    {
+        $expense = new Expense;
+        $expense->category_id = $categoryId;
+        $expense->user_id = $userId;
+        $expense->amount = $amount;
+        $expense->is_shared = false;
+        $expense->plan_id = $planId;
+
+        return $expense->save();
+    }
+
+    /**
+     * Create an equal expense of amount `$amount` for each member of a plan. Use this to save an expense that is shared by every member of a plan.
+     * @param $planId
+     * @param $categoryId
+     * @param $amount
+     * @param $sharedExpenseBatchId
+     * @return bool
+     */
+    public static function createSharedExpenseForAllPlanMembers($planId, $categoryId, $amount, $sharedExpenseBatchId): bool
+    {
+        $planMemberUserIds = PlanMember::getAllPlanMemberUserIds($planId);
+        $result = true;
+        foreach ($planMemberUserIds as $planMemberUserId) {
+            $expense = new Expense;
+            $expense->category_id = $categoryId;
+            $expense->user_id = $planMemberUserId;
+            $expense->amount = $amount;
+            $expense->is_shared = true;
+            $expense->shared_expense_batch_id = $sharedExpenseBatchId;
+            $expense->plan_id = $planId;
+
+            $result = $result && $expense->save();
+        }
+
+        return $result;
+    }
 }
