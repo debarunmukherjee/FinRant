3f1ae3884c8acb8a4f9e5b49d4fab62c8a3b4e1a
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-05 17:22:34 +0530

    Fix route methods and plan route

--- resources/js/Layouts/Authenticated.js
+++ resources/js/Layouts/Authenticated.js
@@ -132,15 +132,13 @@ export default function Authenticated({ auth, header, children }) {
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink
-                            method="post"
                             href={route('dashboard')}
                             active={route().current('dashboard')}
                         >
                             Dashboard
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
-                            method="post"
-                            href={route('dashboard')}
+                            href={route('plans')}
                             active={route().current('plans')}
                         >
                             Plans
