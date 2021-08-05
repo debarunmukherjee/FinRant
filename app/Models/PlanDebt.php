20631332c154e26518e03a8da6c98720147db1ad
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-06 03:23:43 +0530

    Add function to determine user transactions to settle debts

--- app/Models/PlanDebt.php
+++ app/Models/PlanDebt.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use UserAmountMaxHeap;
 
 class PlanDebt extends Model
 {
@@ -67,5 +68,126 @@ class PlanDebt extends Model
         $record->amount = 0;
 
         return $record->save();
+    }
+
+    /**
+     * Returns an associative array containing data of users who are in debt and need to give money to other members of the plan to settle it. Format of every array element is ['userId' => userId, 'amount' => amount]
+     * @param $planId
+     * @return array
+     */
+    public static function getPlanMembersDataWhoOwe($planId): array
+    {
+        $result = self::where([
+            ['plan_id', $planId],
+            ['amount', '>', 0],
+        ])->get(['user_id', 'amount'])->toArray();
+
+        $resultArray = [];
+        foreach ($result as $item) {
+            $resultArray[] = [
+                'userId' => (int)$item['user_id'],
+                'amount' => abs((float) $item['amount'])
+            ];
+        }
+
+        return $resultArray;
+    }
+
+    /**
+     * Returns an associative array containing data of users who are owed money and will receive money from other members of the plan to settle it. Format of every array element is ['userId' => userId, 'amount' => amount]
+     * @param $planId
+     * @return array
+     */
+    public static function getPlanMembersDataWhoAreOwed($planId): array
+    {
+        $result = self::where([
+            ['plan_id', $planId],
+            ['amount', '<', 0],
+        ])->get(['user_id', 'amount'])->toArray();
+
+        $resultArray = [];
+        foreach ($result as $item) {
+            $resultArray[] = [
+                'userId' => (int)$item['user_id'],
+                'amount' => abs((float) $item['amount'])
+            ];
+        }
+
+        return $resultArray;
+    }
+
+    /**
+     * Returns an optimised set of transactions for a subset of members of a plan, to settle all financial lending.
+     * It uses 2 max heaps - one for the users who owe and another for the users who are owed. Then it iteratively
+     * generates a set of transactions until both the heaps are empty. Worst case time complexity is `O(n*log(n))`, where
+     * `n` is the number of members in the plan.
+     *
+     * Return array format : [
+     *      [userId => ['type' => 'pay|receive', 'otherUserId' => otherUserId, 'amount' => amount]]
+     * ]
+     * @param $planId
+     * @return array
+     */
+    public static function getOptimisedTransactionsDataToSettlePlanDebt($planId): array
+    {
+        $debtUserMaxHeap = new UserAmountMaxHeap;
+        $debtUserData = self::getPlanMembersDataWhoOwe($planId);
+        foreach ($debtUserData as $debtUserDatum) {
+            $debtUserMaxHeap->insert($debtUserDatum);
+        }
+
+        $owedUserMaxHeap = new UserAmountMaxHeap;
+        $owedUserData = self::getPlanMembersDataWhoAreOwed($planId);
+        foreach ($owedUserData as $owedUserDatum) {
+            $owedUserMaxHeap->insert($owedUserDatum);
+        }
+
+        $optimisedTransactions = [];
+
+        while (!$debtUserMaxHeap->isEmpty() && !$owedUserMaxHeap->isEmpty()) {
+            $currMaxDebt = $debtUserMaxHeap->extract();
+            $currMaxOwed = $owedUserMaxHeap->extract();
+            if ($currMaxOwed['amount'] > $currMaxDebt['amount']) {
+                $newCurrentDebt = 0;
+                $newCurrentOwe = round($currMaxOwed['amount'] - $currMaxDebt['amount'], 2);
+                $moneyToBePaid = $currMaxDebt['amount'];
+            } else {
+                $newCurrentOwe = 0;
+                $newCurrentDebt = round($currMaxDebt['amount'] - $currMaxOwed['amount'], 2);
+                $moneyToBePaid = $currMaxOwed['amount'];
+            }
+            $areBothHeapsEmpty = $debtUserMaxHeap->isEmpty() && $owedUserMaxHeap->isEmpty();
+            if ($newCurrentOwe > 0) {
+                if (!$areBothHeapsEmpty) {
+                    $owedUserMaxHeap->insert(['userId' => $currMaxOwed['userId'], 'amount' => $newCurrentOwe]);
+                } else {
+                    // Handling edge case where a fraction amount of money is unsettled due to improper division
+                    $moneyToBePaid += $newCurrentOwe;
+                }
+            }
+            if ($newCurrentDebt > 0) {
+                if (!$areBothHeapsEmpty) {
+                    $debtUserMaxHeap->insert(['userId' => $currMaxDebt['userId'], 'amount' => $newCurrentDebt]);
+                } else {
+                    // Handling edge case where a fraction amount of money is unsettled due to improper division
+                    $moneyToBePaid += $newCurrentDebt;
+                }
+            }
+
+            if ($moneyToBePaid > 0) {
+                $optimisedTransactions[$currMaxDebt['userId']][] = [
+                    'type' => 'pay',
+                    'otherUserId' => $currMaxOwed['userId'],
+                    'amount' => $moneyToBePaid
+                ];
+                $optimisedTransactions[$currMaxOwed['userId']][] = [
+                    'type' => 'receive',
+                    'otherUserId' => $currMaxDebt['userId'],
+                    'amount' => $moneyToBePaid
+                ];
+            }
+        }
+
+        return $optimisedTransactions;
     }
 }
