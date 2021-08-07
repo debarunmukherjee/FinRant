e92dcbd26625047f7816f5c9ea73e4b639a16ea7
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-07 17:45:25 +0530

    Add util functions to fetch user expense data

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -65,4 +65,113 @@ class Expense extends Model
 
         return $result;
     }
+
+    /**
+     * Returns the category name and amount for all the unshared expenses of a user in a plan.<br/>
+     * Return array format:
+     *
+     * `[['categoryName' => categoryName, 'amount' => amount]]`
+     * @param $planId
+     * @param $userId
+     * @return array
+     */
+    public static function getAllUnsharedUserExpenses($planId, $userId): array
+    {
+        return self::join('expend_categories', 'expend_categories.id', '=', 'expenses.category_id')
+                ->where([
+                    ['user_id', $userId],
+                    ['plan_id', $planId],
+                    ['is_shared', 0]
+                ])
+                ->orderByDesc('expenses.created_at')
+                ->get(['expend_categories.name as categoryName', 'amount'])->toArray();
+    }
+
+    /**
+     * Returns the category name, amount and share details for all the shared expenses of a user in a plan.<br/>
+     * <br/>Return array format:
+     * ```
+     * [
+     *      [
+     *          'categoryName' => categoryName,
+     *          'amount' => amount,
+     *          'shareDetails` => [['userIdWhoPaid' => userId, 'amount' => amount]]
+     *      ]
+     * ]
+     * ```
+     * @param $planId
+     * @param $userId
+     * @return array
+     */
+    public static function getAllSharedUserExpenses($planId, $userId): array
+    {
+        $expenseData = self::join('expend_categories', 'expend_categories.id', '=', 'expenses.category_id')
+            ->where([
+                ['user_id', $userId],
+                ['plan_id', $planId],
+                ['is_shared', 1]
+            ])
+            ->orderByDesc('expenses.created_at')
+            ->get(['expend_categories.name as categoryName', 'amount', 'shared_expense_batch_id'])->toArray();
+
+        $result = [];
+
+        foreach ($expenseData as $expenseDatum) {
+            $result[] = [
+                'categoryName' => $expenseDatum['categoryName'],
+                'amount' => $expenseDatum['amount'],
+                'shareDetails' => SharedExpenseDetail::getSharedExpenseDetails($expenseDatum['shared_expense_batch_id']),
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns details for all the shared and unshared expenses of a user in a plan in the below format.<br/>
+     * ```
+     * [
+     *      [
+     *          'categoryName' => categoryName,
+     *          'amount' => amount,
+     *          'isShared' => true|false,
+     *          'createdAt' => createdAtTimestamp
+     *          'shareDetails` => [['userIdWhoPaid' => userId, 'amount' => amount]]
+     *      ]
+     * ]
+     * ```
+     * @param $planId
+     * @param $userId
+     * @return array
+     */
+    public static function getAllUserExpenses($planId, $userId): array
+    {
+        $expenseData = self::join('expend_categories', 'expend_categories.id', '=', 'expenses.category_id')
+            ->where([
+                ['user_id', $userId],
+                ['plan_id', $planId]
+            ])
+            ->orderByDesc('expenses.created_at')
+            ->get(['expend_categories.name as categoryName', 'amount', 'shared_expense_batch_id', 'is_shared', 'shared_expense_batch_id', 'expenses.created_at'])->toArray();
+
+        $result = [];
+
+        foreach ($expenseData as $expenseDatum) {
+            $isShared = $expenseDatum['is_shared'];
+            $shareDetails = [];
+            if ($isShared) {
+                $shareDetails = SharedExpenseDetail::getSharedExpenseDetails($expenseDatum['shared_expense_batch_id']);
+            }
+
+            $result[] = [
+                'categoryName' => $expenseDatum['categoryName'],
+                'createdAt' => $expenseDatum['created_at'],
+                'amount' => $expenseDatum['amount'],
+                'isShared' => (int)$isShared === 1,
+                'shareDetails' => $shareDetails
+            ];
+        }
+
+        return $result;
+    }
 }
