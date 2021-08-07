9cbb2ff5b9b5395df3e16ef86520e3a3c9a188b0
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-07 17:23:48 +0530

    Create a global plan context
    - Watch for changes in category list to set the default selected category. This is needed due to the case when the user creates their first category, it should be reflected in the expense record tab.
    - Also updated the selectedExpenseCategory and sharedExpenseMemberToBeAdded states when the parent component gets mounted. This is not done in the child component, because it gets overwritten by the initial state values when the parent component mounts.

--- resources/js/Pages/Plans/ViewPlan.js
+++ resources/js/Pages/Plans/ViewPlan.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useReducer} from "react";
 import Authenticated from "@/Layouts/Authenticated";
 import PlanContainer from "@/Pages/Plans/PlanContainer";
 import SimpleTabs from "@/Components/SimpleTabs";
@@ -7,9 +7,34 @@ import PlanUsers from "@/Pages/Plans/PlanUsers";
 import PlanActivity from "@/Pages/Plans/PlanActivity";
 import RecordPlanExpense from "@/Pages/Plans/RecordPlanExpense";
 import PlanExpenses from "@/Pages/Plans/PlanExpenses";
+import {initialRecordExpenseState, rootReducer} from "@/Pages/Plans/GlobalPlanStates/Reducer";
+
+export const PlanContext = React.createContext();
 
 export default function ViewPlan(props) {
-    const {planDetails} = props;
+    const {planDetails, categoryList, planMemberList} = props;
+    const [state, dispatch] = useReducer(rootReducer, initialRecordExpenseState);
+
+    const setSelectedExpenseCategory = (val) => {
+        dispatch({
+            ...state,
+            selectedExpenseCategory: val
+        });
+    };
+
+    useEffect(() => {
+        if (categoryList.length > 0) {
+            setSelectedExpenseCategory(categoryList[0]);
+        }
+    }, [categoryList]);
+
+    useEffect(() => {
+        dispatch({
+            ...state,
+            selectedExpenseCategory: categoryList.length > 0 ? categoryList[0] : null,
+            sharedExpenseMemberToBeAdded: planMemberList[0]
+        })
+    },[]);
 
     return (
         <Authenticated
@@ -17,37 +42,39 @@ export default function ViewPlan(props) {
             errors={props.errors}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Plan: {planDetails.name}</h2>}
         >
-            <PlanContainer>
-                <div className="m-4 p-6 rounded bg-gray-100">
-                    <span className="font-bold">About: </span> <span className="font-light">{planDetails.description}</span>
-                </div>
-                <SimpleTabs
-                    tabList={
-                        [
-                            {
-                                label: 'Plan Details',
-                                contents: (<PlanDetails id={planDetails.id}/>),
-                            },
-                            {
-                                label: 'Plan Users',
-                                contents: (<PlanUsers id={planDetails.id}/>),
-                            },
-                            {
-                                label: 'Plan Activity',
-                                contents: (<PlanActivity id={planDetails.id}/>),
-                            },
-                            {
-                                label: 'Record Expense',
-                                contents: (<RecordPlanExpense planId={planDetails.id}/>),
-                            },
-                            {
-                                label: 'Plan Expenses',
-                                contents: (<PlanExpenses planId={planDetails.id}/>),
-                            }
-                        ]
-                    }
-                />
-            </PlanContainer>
+            <PlanContext.Provider value={{state, dispatch}}>
+                <PlanContainer>
+                    <div className="m-4 p-6 rounded bg-gray-100">
+                        <span className="font-bold">About: </span> <span className="font-light">{planDetails.description}</span>
+                    </div>
+                    <SimpleTabs
+                        tabList={
+                            [
+                                {
+                                    label: 'Plan Details',
+                                    contents: (<PlanDetails id={planDetails.id}/>),
+                                },
+                                {
+                                    label: 'Plan Users',
+                                    contents: (<PlanUsers id={planDetails.id}/>),
+                                },
+                                {
+                                    label: 'Plan Activity',
+                                    contents: (<PlanActivity id={planDetails.id}/>),
+                                },
+                                {
+                                    label: 'Record Expense',
+                                    contents: (<RecordPlanExpense planId={planDetails.id}/>),
+                                },
+                                {
+                                    label: 'Plan Expenses',
+                                    contents: (<PlanExpenses planId={planDetails.id}/>),
+                                }
+                            ]
+                        }
+                    />
+                </PlanContainer>
+            </PlanContext.Provider>
         </Authenticated>
     );
 }
