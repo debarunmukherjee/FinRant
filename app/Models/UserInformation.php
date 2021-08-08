b1d30f13e653ff2e5a03079e64b161c82b1563e4
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-09 00:43:45 +0530

    Add function to get currency of user

--- app/Models/UserInformation.php
+++ app/Models/UserInformation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PragmaRX\Countries\Package\Countries;
 
 class UserInformation extends Model
 {
@@ -14,4 +15,14 @@ class UserInformation extends Model
         'country',
         'user_id'
     ];
+
+    public static function getUserCurrencyCode($userId): string
+    {
+        $record = self::where('user_id', $userId)->first();
+        if (empty($record)) {
+            return '';
+        }
+        $countries = new Countries();
+        return $countries->where('name.common', $record->country)->first()->currencies->toArray()[0];
+    }
 }
