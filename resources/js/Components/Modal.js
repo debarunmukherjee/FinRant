36e6d1621ae6dc4ace176abf541c02c1f79280c3
Debarun Mukherjee <39825655+debarunmukherjee@users.noreply.github.com>
2021-08-10 00:17:57 +0530

    Remove partial injection of tailwind class names (#13)
    - Refer https://stackoverflow.com/questions/66096170/tailwindcss-missing-colors-in-production-vs-development-laravel-jetstream

--- resources/js/Components/Modal.js
+++ resources/js/Components/Modal.js
@@ -62,7 +62,7 @@ export default function Modal({ open, setOpen, title, children, actionText, onCl
                             <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                                 <button
                                     type="button"
-                                    className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-${isDangerAction ? 'red' : 'blue'}-600 text-base font-medium text-white hover:bg-${isDangerAction ? 'red' : 'blue'}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm`}
+                                    className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 ${isDangerAction ? 'bg-red-600' : 'bg-blue-600'} text-base font-medium text-white ${isDangerAction ? 'hover:bg-red-700' : 'hover:bg-blue-700'} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm`}
                                     onClick={onClickAction}
                                 >
                                     {actionText}
