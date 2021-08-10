6f729210ad18a2760314b65968954c08bb4dc9cb
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-10 13:06:37 +0530

    Add support for custom classes in table component

--- resources/js/Components/Table.js
+++ resources/js/Components/Table.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Table({ headers, children }) {
+export default function Table({ headers, children, customClass = '' }) {
     return (
-        <div className="flex flex-col">
+        <div className={`flex flex-col ${customClass}`}>
             <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
