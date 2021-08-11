4bab7879001078d77fcaf71c90dee66439de7361
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-12 02:13:02 +0530

    Integrate the components in the dashboard

--- resources/js/Pages/Dashboard.js
+++ resources/js/Pages/Dashboard.js
@@ -1,6 +1,8 @@
 import Authenticated from '@/Layouts/Authenticated';
 import React from 'react';
 import PlanExpenseChart from "@/Pages/Dashboard/PlanExpenseChart";
+import MonthlyBudgetVsExpenseChart from "@/Pages/Dashboard/MonthlyBudgetVsExpenseChart";
+import GeneralStats from "@/Pages/Dashboard/GeneralStats";
 
 export default function Dashboard(props) {
     return (
@@ -13,7 +15,9 @@ export default function Dashboard(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
+                            <GeneralStats/>
                             <PlanExpenseChart/>
+                            <MonthlyBudgetVsExpenseChart/>
                         </div>
                     </div>
                 </div>
