80c672f8e7f914d0c9d9b6ffb0b8d9c6da8cb825
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-12 02:12:43 +0530

    Add date related util functions

---
+++ resources/js/Utils/Common.js
@@ -0,0 +1,10 @@
+export function getCurrentMonth() {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const date = new Date();
+    return months[date.getMonth()];
+}
+
+export function getMonthNameFromNumber(monthNumber) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return months[monthNumber-1];
+}
