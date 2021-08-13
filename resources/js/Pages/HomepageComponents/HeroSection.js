2bdb25eceae3cf116c76d43c28f4c4d0c6d326c1
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-13 20:02:16 +0530

    Update video link

--- resources/js/Pages/HomepageComponents/HeroSection.js
+++ resources/js/Pages/HomepageComponents/HeroSection.js
@@ -57,13 +57,13 @@ function HeroHome() {
                                     <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                                     <path d="M10 17l6-5-6-5z" />
                                 </svg>
-                                <span className="ml-3">Watch Demo (2 min)</span>
+                                <span className="ml-3">Watch Demo (15 min)</span>
                             </button>
                         </div>
                         <VideoModal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
                             <div className="relative pb-9/16">
                                 <iframe className="mx-auto w-full h-60-vh sm:h-70-vh"
-                                        src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                                        src="https://www.youtube.com/embed/8URLvVzLTYM">
                                 </iframe>
                             </div>
                         </VideoModal>
