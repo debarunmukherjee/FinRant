51120cdb1c08f1afa4da1d9b67cdfb907e13577f
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2021-08-26 19:00:35 +0530

    Add fusion related environment configs

---
+++ config/finrant.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'fusion_auth_token' => env('FUSION_AUTH_TOKEN', ''),
+    'fusion_funding_ac_id' => env('FUSION_FUNDING_ACCOUNT_ID', ''),
+    'fusion_bundle_id' => env('FUSION_BUNDLE_ID', ''),
+    'fusion_ifi_id' => env('FUSION_IFI_ID', ''),
+    'fusion_bundle_name' => env('FUSION_BUNDLE_NAME', ''),
+];
